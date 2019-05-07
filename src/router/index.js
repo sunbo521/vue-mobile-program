afb26b551a99ac39452d1cf841056cfd706315eb
sunbo <sunbo521github@163.com>
2019-05-07 16:31:08 +0800

    搭建header-tartbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+import HomeContainer from '../components/tabbar/HomeContainer.vue'
+import MemberContainer from '../components/tabbar/MemberContainer.vue'
+import SearchContainer from '../components/tabbar/SearchContainer.vue'
+import ShopcarContainer from '../components/tabbar/ShopcarContainer.vue'
 
+Vue.use(Router)
 export default new Router({
+  linkActiveClass: 'mui-active',
   routes: [
-    {
-      path: '/',
-    }
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/search', component: SearchContainer },
+    { path: '/shopcar', component: ShopcarContainer }
   ]
 })
