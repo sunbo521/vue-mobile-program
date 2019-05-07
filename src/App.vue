3e21ef54f22e19df1d7a78569d9e99adb6d12532
sunbo <sunbo521github@163.com>
2019-05-07 15:48:34 +0800

    搭建-header-tartbar

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- 头部 -->
+    <mt-header fixed title="黑马程序员-Vue项目"></mt-header>
+
+    <router-view/>
+
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">9</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/app.css'
+import './lib/mui/css/icons-extra.css'
+import './lib/mui/fonts/mui.ttf'
+Vue.use(MintUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
